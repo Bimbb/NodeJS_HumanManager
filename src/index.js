bc2a5b586ad6fb90dc502ffec2a412bec7f00de9
DinhLan <ldlan12b7@gmail.com>
2023-03-07 22:27:08 +0700

    update_init

--- src/index.js
+++ src/index.js
@@ -4,24 +4,15 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-
-
-
-
-
 app.use(morgan('combined'));
 const route = require('./routes');
 const db = require('./config/db');
-
 
 app.use(express.json());
 
 db.connect();
 
-
-
-
 route(app);
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-})
+});
